59ae9ca39d4454f569913b9e0cc2cd3d2cf1896c
@mariia.gnutova <m.gnutova@myphotobook.de>
2019-10-30 13:12:36 +0100

    separate navi from main teaser

---
+++ web-app/src/components/Navi.vue
@@ -0,0 +1,26 @@
+<template>
+<nav class = "navbar">
+    <div class="navbar-content">
+    <div class="logo">
+       <!-- svg -->
+      <img src="../assets/logo-horizontal-1.svg"
+       class="logo_horizontal-1"> 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 136 145" class="logo_horizontal-2 mobile">
+        
+        <path class="vc-brand-img-path" d="M3.9,104V40.13L22.4,72l1.1-1.9L5.8,39.83,32.2,55l.9-1.5L4.8,37.33,61.5,4.63l1.9-3.2L2,36.83v70.4C2.5,106.43,3.1,105.33,3.9,104Zm119-64v65.4L71.2,75.53l-1.6,1.1L122.2,107l-57.9,33.4-25-43-1.5,1L62,140l-46.1-26.6-1.7,1.2L63.4,143l61.4-35.4V38.83L122.9,40ZM76.4,8.93l-.9,1.6,35.9,20.7,1.7-1.1ZM45.5,62.73l10.3,6,1.6-1.1-11.1-6.4Z"></path>
+        <path class="vc-brand-img-path" d="M115,31,27.3,90.13,75,7.93l-9.8-5.7c-9.1,15.7-56.4,97.1-61.7,106.2l8.9,5.4c12-8.2,94.5-64.9,112.3-77.1Z"></path>
+        </svg>
+
+      </div>
+    </div>
+  </nav>
+  </template>
+
+  <script>
+export default {
+  name: 'Navi',
+  data(){
+    return{}
+   }
+}
+</script>
--- web-app/src/main.js
+++ web-app/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import './components/style-basics.scss'
+import './components/style-header.scss'
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue'
+import Navi from './components/Navi.vue'
 
 Vue.config.productionTip = false
 
@@ -12,3 +14,7 @@ new Vue({
 new Vue({
   render: h => h(Footer),
 }).$mount('#footer')
+
+new Vue({
+  render: h => h(Navi),
+}).$mount('#navi')
